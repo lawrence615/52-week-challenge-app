74d2a1d1d96e6d518ff3142902adf435d66283d1
Lawrence Macharia <macharialawrence2011@gmail.com>
2017-06-24 08:32:46 +0300

    Updated the menu settings

--- src/components/Navigation/SideNavbar.vue
+++ src/components/Navigation/SideNavbar.vue
@@ -1,9 +1,9 @@
 <template>
     <el-menu class="el-menu-vertical" theme="dark" router>
-        <el-menu-item index="1"><i class="el-icon-menu"></i>Dashboard</el-menu-item>
-        <el-menu-item index="2"><i class="el-icon-setting"></i>Challenges</el-menu-item>
+        <el-menu-item index="1" route="/dashboard"><i class="el-icon-menu"></i>Dashboard</el-menu-item>
+        <el-menu-item index="2" route="/challenges/join"><i class="el-icon-setting"></i>Challenges</el-menu-item>
         <el-menu-item index="3"><i class="el-icon-setting"></i>Invites</el-menu-item>
-        <el-menu-item index="3"><i class="el-icon-setting"></i>Leader board</el-menu-item>
+        <el-menu-item index="4"><i class="el-icon-setting"></i>Leader board</el-menu-item>
     </el-menu>
 </template>
 <script>
