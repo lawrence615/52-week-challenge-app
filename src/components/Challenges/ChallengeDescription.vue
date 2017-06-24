40f973826778e45e06b3866a80969367f20b464a
Lawrence Macharia <macharialawrence2011@gmail.com>
2017-06-24 08:35:13 +0300

    Getting challenge details and sending an event with the data to the parent i.e. Join.vue

--- src/components/Challenges/ChallengeDescription.vue
+++ src/components/Challenges/ChallengeDescription.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="challenge-description">
         <el-row v-if="showDescription">
-            <el-button class="pull-right" type="primary" size="medium">Join</el-button>
-            <br/>
-            <p><strong>Title:</strong> {{ challenge.title }}</p>
+            <p>{{ challenge.title }}
+                <el-button class="pull-right" type="primary" size="medium" @click="joinChallenge(challenge)">Join
+                </el-button>
+            </p>
+            <hr/>
             <p><strong>Description:</strong> {{ challenge.description }}</p>
             <p><strong>Deposit:</strong> {{ challenge.deposit | currency('Ksh. ') }}</p>
             <p><strong>Total:</strong> {{ challenge.total | currency('Ksh. ') }}</p>
             <p><strong>Start date:</strong>
-                <el-date-picker v-model="start_date" type="date" placeholder="Pick start date"></el-date-picker>
+                <el-date-picker @change="onStartDateChange" v-model="start_date" type="date"
+                                placeholder="Pick start date"></el-date-picker>
             </p>
             <p><strong>End date:</strong>
                 &nbsp;
-                <el-date-picker v-model="end_date" type="date" placeholder="Pick start date"></el-date-picker>
+                {{ endDate }}
             </p>
         </el-row>
         <el-row v-else>
@@ -29,12 +32,29 @@
     export default{
         name: 'challenge-description',
         props: ['challenge'],
-        computed: null,
         data: function () {
             return {
                 showDescription: false,
                 start_date: '',
-                end_date: ''
+                end_date: '',
+                pickStartDate: {
+                    shortcuts: [{
+                        text: 'Today',
+                        onClick(picker){
+                            picker.$emit('pick', new Date())
+                        }
+                    }
+                    ]
+                },
+                challenge_data: {}
+            }
+        },
+        computed: {
+            endDate(){
+                if (this.end_date == null) {
+                    return;
+                }
+                return moment(this.end_date).add(52, "weeks").format("YYYY-MM-D");
             }
         },
         watch: {
@@ -49,8 +69,23 @@
             }
         },
         methods: {
-            joinChallenge(){
+            joinChallenge(challenge){
 
+                this.challenge_data = {
+                    'challenge_id': challenge.key,
+                    'start_date': moment(this.start_date).format("YYYY-MM-D"),
+                    'end_date': this.endDate,
+                    'increment': challenge.deposit,
+                    'title': challenge.title,
+                };
+
+                this.$emit('challenge-joined', this.challenge_data);
+            },
+            onStartDateChange(date){
+                if (date == null) {
+                    return;
+                }
+                this.end_date = date;
             }
         }
     }
